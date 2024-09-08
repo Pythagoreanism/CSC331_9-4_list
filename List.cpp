9dcc375f0191be59b5c1347b7229cf3967c8abbf
Jason Palma <jasonpalma16@gmail.com>
2024-09-08 18:51:06 -0400

    Add class List documentation
    Documentation consists of the Doxygen-style explanations for member variales and functions

--- List.cpp
+++ List.cpp
@@ -9,10 +9,29 @@ template <class Type>
 void List<Type>::setMax(int m) { max = m; }
 template <class Type>
 int List<Type>::getMax() const { return max; }
+/**
+ * @brief Checks if the list's length reached the maximum value of the list
+ * @return Boolean
+ * @retval true If length is the same as max
+ * @retval false If length is not the same as max
+ */
 template <class Type>
 bool List<Type>::isFull() const { return length == max; }
+/**
+ * @brief Checks if the list's is empty
+ * @return Boolean
+ * @retval true If length is 0
+ * @retval false If length is not 0
+ */
 template <class Type>
 bool List<Type>::isEmpty() const { return length == 0; }
+/**
+ * @brief Inserts an item to the list
+ * @details Checks if the list is full, if not then it will append the item
+ * to the list
+ * @param item The item to be inserted into the list
+ * @return void
+ */
 template <class Type>
 void List<Type>::insertItem(Type item) {
     if (length == max) {
@@ -23,6 +42,15 @@ void List<Type>::insertItem(Type item) {
         std::cout << "Item inserted succesfully." << std::endl;
     }
 }
+/**
+ * @brief Deletes an item of the list
+ * @details This function deletes an item in a simple but technical way.
+ * It searches for the item being passed and checks if the item is in the list.
+ * If the item is in the list, it will then take the last item of the list, 
+ * replace the item that is being "deleted", and decrements the length by 1.
+ * @param item The item being "deleted"
+ * @return void
+ */
 template <class Type>
 void List<Type>::deleteItem(Type item) {
     if (length == 0) {
@@ -51,12 +79,23 @@ void List<Type>::deleteItem(Type item) {
         }
     }
 }
+/**
+ * @brief Prints the contents of the list
+ * @return void
+ */
 template <class Type>
 void List<Type>::printList() const {
     for (unsigned int i = 0; i < length; i++) {
         std::cout << "Item " << i + 1 << ": " << data[i] << std::endl;
     }
 }
+/**
+ * @brief A function that searches an item of the list
+ * @param item The value that's being searched for
+ * @return Boolean
+ * @retval true If the item is found
+ * @retval false If the item is not found
+ */
 template <class Type>
 bool List<Type>::searchItem(Type item) const {
     for (unsigned int i = 0; i < length; i++) {
@@ -67,32 +106,53 @@ bool List<Type>::searchItem(Type item) const {
 
     return false;
 }
-
+/**
+ * @brief Default constructor for List
+ * @details Constructed using the intializer list for readable code
+ * @param size The maximum size to initialize the list. By default size is 100
+ */
 template <class Type>
 List<Type>::List(int size) : length(0), max(size), data(new Type[max]) {}
+/**
+ * @brief Destructor for List
+ */
 template <class Type>
 List<Type>::~List() { delete [] data; }
+/**
+ * @brief Copy constructor for List
+ * @param other This is the object being copied from
+ */
 template <class Type>
-List<Type>::List(const List<Type>& right) {
-    length = right.length;
-    max = right.max;
+List<Type>::List(const List<Type>& other) {
+    length = other.length;
+    max = other.max;
     data = new Type[max];
     for (unsigned int i = 0; i < length; i++) { // Copies array contents
-        data[i] = right.data[i];
+        data[i] = other.data[i];
     }
 }
+/**
+ * @brief Operator overload for the = operator
+ * @details The function first checks if the object being copied from isn't
+ * the same object copying it, then it follows the same copying process as 
+ * the copy constructor
+ * @see List(const List<Type> & other)
+ * @param right This is the object being copied from, its on the right
+ * side of the = operator
+ * @return An instance of the List class with the contents of the object being
+ * assigned from
+ */
 template <class Type>
-const List<Type>& List<Type>::operator=(const List<Type>& other) {
-    
-    if (this == &other) {
+const List<Type>& List<Type>::operator=(const List<Type>& right) {
+    if (this == &right) {
         return *this;
     }
     else {
-        length = other.length;
-        max = other.max;
+        length = right.length;
+        max = right.max;
         data = new Type[max];
         for (unsigned int i = 0; i < length; i++) { 
-            data[i] = other.data[i];
+            data[i] = right.data[i];
         }
     }
     
--- List.h
+++ List.h
@@ -1,11 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 
+/**
+ * @class List
+ * @brief A class that simulates a list data structure
+ * @details The List is a template class that has several functions to match that of a list such as 
+ * adding, removing, checking if its full, empty.
+ */
 template <class Type>
 class List {
 private:
+    /**
+     * @var length
+     * @brief The current length of the list
+     */
     int length;
+    /**
+     * @var max
+     * @brief The maximum value that the length can reach.
+     */
     int max;
+    /**
+     * @var *data
+     * @brief A pointer that is assigned to an array at runtime
+     */
     Type* data;
 
 public:
